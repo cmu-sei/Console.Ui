f758c55a0c70e30524c1877e6769a7403b0fb39b
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-02-15 15:49:21 -0500

    install localize

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
